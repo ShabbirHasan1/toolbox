9b3c1e8fd799556c4ce33188a28e0ab29e0d202c
keang <skeang@gmail.com>
2016-09-19 01:38:41 +0800

    Fix test toolbox

--- test_toolbox.py
+++ test_toolbox.py
@@ -1,3 +1,4 @@
+import pytest
 from zipline import TradingAlgorithm
 from zipline.testing.fixtures import (
     WithDataPortal,
@@ -18,21 +19,45 @@ class ToolboxTestCase(WithDataPortal,
     def test_create_order(self):
 
         def initialize(context):
-            context.broker = Client("backtest", context.asset_finder)
+            context.broker = Client(context, "backtest", context.asset_finder)
 
         def handle_data(context, data):
             # Asset 'A' has price increasing linearly from 10 to 260
             # Asset 'B' has price increasing linearly from 11 to 261
             # Asset 'C' has price increasing linearly from 12 to 262
-            a = symbol('A')
-            context.ordered = False
+            a, b, c = symbol('A'), symbol('B'), symbol('C')
+            if not hasattr(context, "ordered"):
+                context.ordered = False
             if not context.ordered:
-                context.broker.create_order(a, 100, limit=10.1,
-                                            stop_loss=9.00, take_profit=11.00)
                 context.ordered = True
+                context.broker.create_order(a, 100, limit=270)  # remain open
+                context.broker.create_order(a, 100, stop=20)   # filled
+                """
+                context.broker.create_order(a, 100, limit=100,
+                                            take_profit=102,
+                                            stop_loss=98)   # filled, profitted
+
+                context.broker.create_order(b, -100, limit=200,
+                                            take_profit=198,
+                                            stop_loss=202)   # filled, loss
+
+                context.broker.create_order(b, -100, limit=200,
+                                            take_profit=198,
+                                            stop_loss=302)   # filled, remain open
+
+                context.broker.create_order(c, -100, limit=200,
+                                            take_profit=188,
+                                            stop_loss=212)   # filled, early closed
+
+            if data[c].price == 205:
+                context.broker.create_order(c, 100, limit=200,
+                                            take_profit=212,
+                                            stop_loss=188)   # closes earlier bracket order
+                                            """
 
         def analyze(context, perf):
-            assert len(context.broker.open_orders) == 1
+            pytest.set_trace()
+            assert len(context.broker.open_orders[symbol('A')]) == 1
 
         algo = TradingAlgorithm(initialize=initialize,
                                 handle_data=handle_data,
