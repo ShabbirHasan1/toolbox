def1f2f0640d4ecd1be3bfcb850887851d02b1ac
keang <skeang@gmail.com>
2016-09-19 04:11:10 +0800

    Add specs

--- test_toolbox.py
+++ test_toolbox.py
@@ -19,7 +19,7 @@ class ToolboxTestCase(WithDataPortal,
     def test_create_order(self):
 
         def initialize(context):
-            context.broker = Client(context, "backtest", context.asset_finder)
+            context.blotter = Client(context, "backtest", context.asset_finder)
 
         def handle_data(context, data):
             # Asset 'A' has price increasing linearly from 10 to 260
@@ -30,34 +30,40 @@ class ToolboxTestCase(WithDataPortal,
                 context.ordered = False
             if not context.ordered:
                 context.ordered = True
-                context.broker.create_order(a, 100, limit=270)  # remain open
-                context.broker.create_order(a, 100, stop=20)   # filled
-                """
-                context.broker.create_order(a, 100, limit=100,
-                                            take_profit=102,
-                                            stop_loss=98)   # filled, profitted
 
-                context.broker.create_order(b, -100, limit=200,
-                                            take_profit=198,
-                                            stop_loss=202)   # filled, loss
+                context.blotter.create_order(a, 1, stop=100,
+                                             take_profit=102,
+                                             stop_loss=98)   # filled, profitted
 
-                context.broker.create_order(b, -100, limit=200,
-                                            take_profit=198,
-                                            stop_loss=302)   # filled, remain open
+                context.blotter.create_order(b, -1, limit=200,
+                                             take_profit=198,
+                                             stop_loss=202)   # filled, loss
 
-                context.broker.create_order(c, -100, limit=200,
-                                            take_profit=188,
-                                            stop_loss=212)   # filled, early closed
+                context.blotter.create_order(b, -100, limit=200,
+                                             take_profit=198,
+                                             stop_loss=302)   # filled, remain open
+
+                context.blotter.create_order(c, -100, limit=200,
+                                             take_profit=188,
+                                             stop_loss=212)   # filled, early closed
 
             if data[c].price == 205:
-                context.broker.create_order(c, 100, limit=200,
-                                            take_profit=212,
-                                            stop_loss=188)   # closes earlier bracket order
-                                            """
+                context.blotter.create_order(c, 100, limit=206,
+                                             take_profit=212,
+                                             stop_loss=188)   # closes earlier bracket order
 
         def analyze(context, perf):
-            pytest.set_trace()
-            assert len(context.broker.open_orders[symbol('A')]) == 1
+            assert len(context.blotter.open_orders[symbol('A')]) == 0
+            assert len(context.blotter.open_orders[symbol('B')]) == 1
+            assert len(context.blotter.open_orders[symbol('C')]) == 0
+
+            assert len(context.blotter.profit_orders[symbol('A')]) == 1
+            assert len(context.blotter.profit_orders[symbol('B')]) == 0
+            assert len(context.blotter.profit_orders[symbol('C')]) == 1
+
+            assert len(context.blotter.loss_orders[symbol('A')]) == 0
+            assert len(context.blotter.loss_orders[symbol('B')]) == 1
+            assert len(context.blotter.loss_orders[symbol('C')]) == 0
 
         algo = TradingAlgorithm(initialize=initialize,
                                 handle_data=handle_data,
@@ -65,3 +71,4 @@ class ToolboxTestCase(WithDataPortal,
                                 sim_params=self.sim_params,
                                 env=self.env)
         algo.run(self.data_portal)
+
