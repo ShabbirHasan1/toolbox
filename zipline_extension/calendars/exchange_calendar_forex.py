132a10f38c6c5d29c38a388af7d50e7ceb71e8fa
keang <skeang@gmail.com>
2016-10-02 23:15:55 +0800

    Add weekend close and open times

--- zipline_extension/calendars/exchange_calendar_forex.py
+++ zipline_extension/calendars/exchange_calendar_forex.py
@@ -1,8 +1,13 @@
 import pytz
+import pandas as pd
 from datetime import time
 from zipline.utils.calendars import TradingCalendar
 
+
 class ForexCalendar(TradingCalendar):
+
+    NYT_5PM = time(9)
+
     @property
     def name(self):
         return "forex"
@@ -18,3 +23,23 @@ class ForexCalendar(TradingCalendar):
     @property
     def close_time(self):
         return time(23, 59)
+
+    def special_opens_adhoc(self):
+        return [
+            (self.NYT_5PM, self._sunday_dates())
+        ]
+
+    def special_closes_adhoc(self):
+        return [
+            (self.NYT_5PM, self._friday_dates())
+        ]
+
+    def _friday_dates(self):
+        return pd.date_range(start=self.schedule.index[0],
+                             end=self.schedule.idnex[-1],
+                             freq='W-FRI')
+
+    def _sunday_dates(self):
+        return pd.date_range(start=self.schedule.index[0],
+                             end=self.schedule.idnex[-1],
+                             freq='W-SUN')
