faa3c8b98741db2e9893b3cc4a7304e14beabb12
keang <skeang@gmail.com>
2016-10-04 00:02:00 +0800

    Fix forex calendar

--- zipline_extension/calendars/exchange_calendar_forex.py
+++ zipline_extension/calendars/exchange_calendar_forex.py
@@ -1,12 +1,23 @@
+import pytest
 import pytz
 import pandas as pd
 from datetime import time
 from zipline.utils.calendars import TradingCalendar
+from zipline.utils.memoize import lazyval
 
 
 class ForexCalendar(TradingCalendar):
 
-    NYT_5PM = time(9)
+    NYT_4PM = time(20)
+    NYT_6PM = time(22)
+
+    @lazyval
+    def day(self):
+        return pd.tseries.offsets.CustomBusinessDay(
+            holidays=self.adhoc_holidays,
+            calendar=self.regular_holidays,
+            weekmask='Sun Mon Tue Wed Thu Fri'
+        )
 
     @property
     def name(self):
@@ -24,22 +35,38 @@ class ForexCalendar(TradingCalendar):
     def close_time(self):
         return time(23, 59)
 
-    def special_opens_adhoc(self):
+    def _calculate_special_opens(self, start, end):
+        return self._special_dates(
+            self.special_opens,
+            self.special_opens_adhoc(start, end),
+            start,
+            end,
+        )
+
+    def _calculate_special_closes(self, start, end):
+        return self._special_dates(
+            self.special_closes,
+            self.special_closes_adhoc(start, end),
+            start,
+            end,
+        )
+
+    def special_opens_adhoc(self, start, end):
         return [
-            (self.NYT_5PM, self._sunday_dates())
+            (self.NYT_6PM, self._sunday_dates(start, end))
         ]
 
-    def special_closes_adhoc(self):
+    def special_closes_adhoc(self, start, end):
         return [
-            (self.NYT_5PM, self._friday_dates())
+            (self.NYT_4PM, self._friday_dates(start, end))
         ]
 
-    def _friday_dates(self):
-        return pd.date_range(start=self.schedule.index[0],
-                             end=self.schedule.idnex[-1],
+    def _friday_dates(self, start, end):
+        return pd.date_range(start=start,
+                             end=end,
                              freq='W-FRI')
 
-    def _sunday_dates(self):
-        return pd.date_range(start=self.schedule.index[0],
-                             end=self.schedule.idnex[-1],
+    def _sunday_dates(self, start, end):
+        return pd.date_range(start=start,
+                             end=end,
                              freq='W-SUN')
