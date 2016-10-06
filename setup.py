f0148d1c7e9a97bb3d5647cbdda2e89299157e93
keang <skeang@gmail.com>
2016-10-07 07:41:31 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name="ziprex_toolbox",
+    name="forex_toolbox",
 
     # Version number (initial):
     version="0.1.0",
@@ -20,18 +20,23 @@ setup(
     # Details
     url="http://github.com/bernoullio/toolbox",
 
-    #
-    # license="LICENSE.txt",
     description="Useful stuff for running forex backtest using zipline",
 
     long_description=open("README.md").read(),
 
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+
     # Dependent packages (distributions)
     install_requires=[
-        'pandas',
-        'zipline',
+        'pandas>=0.16',
+        'zipline>1.0',
         'logbook',
         'psycopg2',
+        'oandapy',
+    ],
+    dependency_links=[
         'git+git://github.com/oanda/oandapy@master#egg=oandapy'
     ],
 )
